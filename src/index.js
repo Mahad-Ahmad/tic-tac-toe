8c5fa2ab47d1cf650f897e2a12d1f7c8f3275e66
Mahad Ahmad <mahadahmad921@gmail.com>
2021-10-20 12:13:44 +0500

    update: index.js file added

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import Game from "./components/game";
+function App() {
+  return <Game />;
+}
+ReactDOM.render(<App />, document.getElementById("root"));
+
+export default App;
