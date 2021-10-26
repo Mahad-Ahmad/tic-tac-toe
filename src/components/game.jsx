bad54309c60be0c2f65bb10c1af9301ed02d8fcf
Mahad Ahmad <mahadahmad921@gmail.com>
2021-10-26 12:22:02 +0500

    update: game file added

---
+++ src/components/game.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+import "../index.css";
+function Game() {
+  const [state, setState] = useState({
+    history: [
+      {
+        squares: Array(9).fill(null),
+      },
+    ],
+  });
+  const [xIsNext, setXIsNext] = useState(true);
+  const [stepNum, setStepNum] = useState(0);
+  const history = state.history;
+  const current = history[stepNum];
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  const winner = calculateWinner(current.squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  function jumpTo(step) {
+    setStepNum(step);
+    setXIsNext(step % 2 === 0);
+  }
+  return (
+    <div className="game">
+      <div className="game-board">
+       
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
